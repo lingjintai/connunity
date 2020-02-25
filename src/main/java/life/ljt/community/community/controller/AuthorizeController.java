f666e2b0708275bfad00d7f7bda3fa114bb6cbec
lingjintai <13156699719@163.com>
2020-02-25 10:47:07 +0800

    ljt

--- src/main/java/life/ljt/community/community/controller/AuthorizeController.java
+++ src/main/java/life/ljt/community/community/controller/AuthorizeController.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -36,8 +38,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state,
-    HttpServletRequest request
-    ) {
+                           HttpServletRequest request, HttpServletResponse response
+                           ) {
 
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setClient_id(clientId);
@@ -49,13 +51,15 @@ public class AuthorizeController {
         GithubUser user = githubProvider.getUser(accessToken);
         if (user!=null){
             User user1 =new User();
-            user1.setToken(UUID.randomUUID().toString());
+            String token=UUID.randomUUID().toString();
+            user1.setToken(token);
             user1.setName(user.getName());
             user1.setAccountId(String.valueOf(user.getId()));
             user1.setGmtCreate(System.currentTimeMillis());
             user1.setGmtModified(user1.getGmtCreate());
             userMapper.addUser(user1);
             //就写登录的cookie和session
+            response.addCookie(new Cookie("token",token));
             request.getSession().setAttribute("user",user);
             return "redirect:/";
         }else{
--- src/main/java/life/ljt/community/community/mapper/UserMapper.java
+++ src/main/java/life/ljt/community/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package life.ljt.community.community.mapper;
 import life.ljt.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
@@ -11,4 +13,6 @@ public interface UserMapper {
     @Insert("insert  into  USER(NAME ,ACCOUNT_ID,TOKEN,GMT_CREATE,GMT_MODIFIED) values (#{name},#{accountId},#{token} ,#{gmtCreate} ,#{gmtModified}  )")
     void addUser(User user);
 
+    @Select("select * from USER where TOKEN=#{token} ")
+    User findByToken(@Param("token") String token);
 }
